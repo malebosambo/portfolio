ab4743d457ebd22bfcb14332c76ab31014ea4a3f
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-01-19 10:06:54 +0200

    Update App.js
    First commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Malebo Sambo Portfolio
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+          My first React project.
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+
       </header>
     </div>
   );
