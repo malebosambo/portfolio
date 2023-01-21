7b8faebdd1f823341d3574f7253765ad2f005dd5
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-01-21 11:50:01 +0200

    Adding call to action links

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ function App() {
           Malebo Sambo Portfolio
         </p>
         <p>
-          View my projects on GitHub
+          View my projects on GitHub <a href="https://github.com/malebosambo">here</a>
         </p>
         
         <div>
-          <input type="submit" value="Resume"></input>
+          <input type="submit" value="Networking"></input>
+          <input type="submit" value="Websites"></input>
+          <input type="submit" value="Mobile Apps"></input>
+          <input type="submit" value="IoT"></input>
         </div>
 
       </header>
