06135f83180ff9b62f91211259a95ed8d6c63eb1
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-02-06 14:57:15 +0200

    Site title design

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
 
         <div className="site_nav">
           <div id="title">
-            <p>Malebo Sambo</p>
-          </div>
-          <div id="menu">
-            <ul>
-              <li><a href="www.google.com">GitHub</a></li>
-              <li><a href="www.google.com">Websites</a></li>
-              <li><a href="www.google.com">Apps</a></li>
-              <li><a href="www.google.com">IoT</a></li>
-              <li><a href="www.google.com">Networking</a></li>
-              <li><a href="www.google.com">Contact</a></li>
-            </ul>
+            <p className="Title">Malebo Sambo</p>
           </div>
         </div>
 
