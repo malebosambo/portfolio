79c6e9224afb4797b809afa13c9b5dbd531240c1
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-03-11 13:42:04 +0200

    Testing the login component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomeContentCont from './components/homeContentCont';
+import Login from './components/login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomeContentCont />
+    <Login />
   </React.StrictMode>
 );
 
