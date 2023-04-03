d6e466b7717aff0ce285cb8a4a36ccae4f411c6d
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-04-03 09:09:57 +0200

    Changed entry point component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './components/login';
+import HomeContentCont from './components/homeContentCont';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <HomeContentCont />
   </React.StrictMode>
 );
 
