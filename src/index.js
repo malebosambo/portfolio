b7063102738c5e097aa674a0ebeabed61ce7c627
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-05-12 13:57:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/CSS/bootstrap.css';
 import { App, Github, Websites, Apps, IoT, Network, Contact, Login } from './App';
 import {
   BrowserRouter,
