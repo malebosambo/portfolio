bde8f925cbf66ae42652204719fcc3bb26e0d2dc
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-05-12 13:03:17 +0200

    Update homeContentCont.js

--- src/components/homeContentCont.js
+++ src/components/homeContentCont.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './homeContentCont.css';
 import LatestIssues from './latestIssues';
 import ContentItem from './contentitem';
