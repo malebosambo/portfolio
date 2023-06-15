93ee41cae0dbb21d897a265639234f12f49bb946
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-06-15 11:29:01 +0200

    Removed back end files

--- config/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || '3000',
-  jwtSecret: process.env.JWT_SECRET || 'YOUR_JWT_SECRET'
-}
-
-export default config;
