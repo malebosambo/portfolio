d4ab8797735341648a9edc4f081936c8e83240d8
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-08-28 13:58:57 +0200

    Update issueitem.js

--- src/components/issueitem.js
+++ src/components/issueitem.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class IssueItem extends Component {
-  render() {
-      const {issue} = this.props;
+const issueItem = (props) {
+  const {issue} = this.props;
     
-    return (
-      <>
-        <tr key={issue.id}>
-          <td>{issue.title}</td>
-          <td>{issue.body}</td>
-          <td>{issue.assignees[0]}</td>
-          <td>{issue.state}</td>
-        </tr>
-      </>
-    )
-  }
+  return (
+    <>
+      <tr key={issue.id}>
+        <td>{issue.title}</td>
+        <td>{issue.body}</td>
+        <td>{issue.assignees[0]}</td>
+        <td>{issue.state}</td>
+      </tr>
+    </>
+  )
 }
-
-export default IssueItem;
