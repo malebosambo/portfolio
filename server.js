aa1db7acd3b87ce9325ad5c7cb38980b03282eff
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-09-06 15:11:48 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000;
+app.listen(HTTP_PORT, () => {
+  console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT)
+)};
+
+app.get("/", (req, res, next) => {
+  res.json({ "message": "OK" });
+});
+
+app.use(function (req, res) {
+  res.status(404);
+});
