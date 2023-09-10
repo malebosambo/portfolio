672406a77a375aa6c6009b11449fdc7368cafeff
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-09-10 17:48:00 +0200

    Update footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Footer() {
+
+  return (
+    <>
+      <div>
+        <p>Malebo Sambo<\p>
+        <p>© All rights reserved.<\p>
+      <\div>
+      <div>
+        <p>Email: help@malebo.sambo.com<\p>
+        <p>Cell: +27630013711<\p>
+      <\div>
+    <\>
+  );
+}
