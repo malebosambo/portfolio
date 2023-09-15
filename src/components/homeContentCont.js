ad29d5ad30e1f0f387baba98a23f6c9539b1f795
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-09-15 18:13:02 +0200

    Update homeContentCont.js

--- src/components/homeContentCont.js
+++ src/components/homeContentCont.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './homeContentCont.css';
 import LatestIssues from './latestIssues';
 import ContentItem from './contentitem';
 
-class HomeContentCont extends Component {
+export default function HomeContentCont() {
   state = {
     services : [
       {
@@ -44,26 +44,21 @@ class HomeContentCont extends Component {
     ]
   };
   
-  render() {
+  const content = this.state.services.map((service) => <ContentItem content={service} />);
 
-    const content = this.state.services.map((service) => <ContentItem content={service} />);
-
-    return (
-      <div className="Container">
+  return (
+    <div className="Container">
         
-        <div className="container">
-          <div className="row">
-            {content}
-          </div>
+      <div className="container">
+        <div className="row">
+          {content}
         </div>
-        
-        <div className="Latest_Issues">
-          <LatestIssues />
-        </div>
-        
       </div>
-    )
-  }
+        
+      <div className="Latest_Issues">
+        <LatestIssues />
+      </div>
+        
+    </div>
+  )
 }
-
-export default HomeContentCont;
