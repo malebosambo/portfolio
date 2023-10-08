e038454d6ce30b15798ed2af0f2af99ee34fd0d2
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-10-08 09:28:54 +0200

    Update latestIssues.js

--- src/features/latestIssues.js
+++ src/features/latestIssues.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import IssueItem from './issueItem';
-import './latestIssues.css';
-import './../App.css';
+import '../styles/latestIssues.css';
+import '../App.css';
 
 class LatestIssues extends Component {
   state = {
