50690d62964199b928ab12221ec6ebed5b8245bd
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-10-08 14:22:10 +0200

    Update home.js

--- src/features/home.js
+++ src/features/home.js
@@ -8,38 +8,24 @@ export default function Home() {
   const services = [
     {
       id: 1,
-      name: "GitHub",
+      name: "GitHub Profile",
       ops: ["DevOps", "Virtualization", "Security"],
       imgUrl: "./GitHub.png",
       url: "/github"
     },
     {
       id: 2,
-      name: "Websites",
+      name: "Web Apps",
       ops: ["Fintech", "eCommerce", "AI", "Community"],
       imgUrl: "./Web-App.jpg",
       url: "/websites"
     },
     {
       id: 3,
-      name: "Apps",
+      name: "Mobile Apps",
       ops: ["Fintech", "eCommerce", "AI", "Community"],
       imgUrl: "./Mobile-App.png",
       url: "/apps"
-    },
-    {
-      id: 4,
-      name: "IoT",
-      ops: ["Security", "Utility"],
-      imgUrl: "./Edge-Computing.png",
-      url: "/iot"
-    },
-    {
-      id: 5,
-      name: "Network",
-      ops: ["Internet", "Cybersecurity"],
-      imgUrl: "./Networking.jpg",
-      url: "/network"
     }
   ];
 
