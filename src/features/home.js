9b2ffba7d2ce495d6e7a0a6c1ce9f45f4c3bdd4d
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-10-08 08:04:31 +0200

    Update home.js

--- src/features/home.js
+++ src/features/home.js
@@ -1,69 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './homeContentCont.css';
 import LatestIssues from '../components/latestIssues';
 import ContentItem from '../components/contentitem';
 
-class HomeContentCont extends Component {
-  state = {
-    services : [
-      {
-        id: 1,
-        name: "GitHub",
-        ops: ["DevOps", "Virtualization", "Security"],
-        imgUrl: "./GitHub.png",
-        url: "/github"
-      },
-      {
-        id: 2,
-        name: "Websites",
-        ops: ["Fintech", "eCommerce", "AI", "Community"],
-        imgUrl: "./Web-App.jpg",
-        url: "/websites"
-      },
-      {
-        id: 3,
-        name: "Apps",
-        ops: ["Fintech", "eCommerce", "AI", "Community"],
-        imgUrl: "./Mobile-App.png",
-        url: "/apps"
-      },
-      {
-        id: 4,
-        name: "IoT",
-        ops: ["Security", "Utility"],
-        imgUrl: "./Edge-Computing.png",
-        url: "/iot"
-      },
-      {
-        id: 5,
-        name: "Network",
-        ops: ["Internet", "Cybersecurity"],
-        imgUrl: "./Networking.jpg",
-        url: "/network"
-      }
-    ]
-  };
-  
-  render() {
+export default function HomeContentCont() {
 
-    const content = this.state.services.map((service) => <ContentItem content={service} />);
+  services : [
+    {
+      id: 1,
+      name: "GitHub",
+      ops: ["DevOps", "Virtualization", "Security"],
+      imgUrl: "./GitHub.png",
+      url: "/github"
+    },
+    {
+      id: 2,
+      name: "Websites",
+      ops: ["Fintech", "eCommerce", "AI", "Community"],
+      imgUrl: "./Web-App.jpg",
+      url: "/websites"
+    },
+    {
+      id: 3,
+      name: "Apps",
+      ops: ["Fintech", "eCommerce", "AI", "Community"],
+      imgUrl: "./Mobile-App.png",
+      url: "/apps"
+    },
+    {
+      id: 4,
+      name: "IoT",
+      ops: ["Security", "Utility"],
+      imgUrl: "./Edge-Computing.png",
+      url: "/iot"
+    },
+    {
+      id: 5,
+      name: "Network",
+      ops: ["Internet", "Cybersecurity"],
+      imgUrl: "./Networking.jpg",
+      url: "/network"
+    }
+  ];
 
-    return (
-      <div className="Container">
+  const content = services.map((service) => <ContentItem content={service} />);
+
+  return (
+    <div className="Container">
         
-        <div className="container">
-          <div className="row">
-            {content}
-          </div>
+      <div className="container">
+        <div className="row">
+          {content}
         </div>
-        
-        <div className="Latest_Issues">
-          <LatestIssues />
-        </div>
-        
       </div>
-    )
-  }
+        
+      <div className="Latest_Issues">
+        <LatestIssues />
+      </div>
+        
+    </div>
+  )
 }
-
-export default HomeContentCont;
