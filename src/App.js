2020716f6f584868943b386483d9c9fe9f3bdcd7
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-10-08 08:31:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Link} from 'react-router-dom';
 import Home from './features/home';
-import Login from './security/login';
 import GithubProfile from './features/githubProfile';
 import WebsiteApps from './features/websiteApps';
 import MobileApps from './features/mobileApps';
@@ -118,29 +117,6 @@ export function Contact() {
         </nav>
       </div>
       <ContactComp />
-    </div>
-  )
-}
-
-export function Login() {
-  return (
-    <div>
-      <div className="Header">
-        <div className="Logo">
-          <p>Malebo Sambo</p>
-        </div>
-        <nav>
-          <Link to="/home">Home</Link>
-          <Link to="#">GitHub</Link>
-          <Link to="/websites">Websites</Link>
-          <Link to="/apps">Apps</Link>
-          <Link to="/iot">IoT</Link>
-          <Link to="/network">Network</Link>
-          <Link to="/contact">Contact Us</Link>
-          <Link to="/login">Login</Link>
-        </nav>
-      </div>
-      <LoginComp />
     </div>
   )
 }
