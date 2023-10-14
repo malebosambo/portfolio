f03a828dbcba768ce472ec077d4554e3bf910809
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-10-14 18:34:42 +0200

    Update websiteApps.js

--- src/features/websiteApps.js
+++ src/features/websiteApps.js
@@ -8,22 +8,6 @@ class Websites extends Component {
       
         <h1>Website Development</h1>
         
-        <div>
-          <ul>
-            <li><label htmlFor="hosting">Hosting</label><select id="hosting" name="hosting"><option value="SHP">Self-Hosted Provider</option><option value="cloud">Cloud</option></select></li>
-            <li><label htmlFor="framework">Framework</label><select id="framework" name="framework"><option value="express">Express.js</option><option value="react">React.js</option><option value="wordpress">Wordpress</option></select></li>
-          </ul>
-        </div>
-        
-        <div className="Website">
-          <div className="Website_Iframe">
-            <h2>Billing Portal</h2>
-            
-          </div>
-          <div className="Website_Details">
-            <h3>Project Details</h3>
-          </div>
-        </div>
       </div>
     );
   }
