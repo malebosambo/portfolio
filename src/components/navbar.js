8f363ae6001610af701682afca52389f476e4ddc
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-10-14 18:39:06 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,12 +10,12 @@ function NavbarComp() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/github">GitHub</Nav.Link>
-            <Nav.Link href="/websites">Websites</Nav.Link>
-            <Nav.Link href="/apps">Apps</Nav.Link>
+            <Nav.Link href="/github">GitHub Stats</Nav.Link>
+            <Nav.Link href="/websites">Web-Apps</Nav.Link>
+            <Nav.Link href="/apps"> Mobile-Apps</Nav.Link>
             <Nav.Link href="/iot">IoT</Nav.Link>
             <Nav.Link href="/network">Networks</Nav.Link>
-            <Nav.Link href="/contact">Contacts</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
