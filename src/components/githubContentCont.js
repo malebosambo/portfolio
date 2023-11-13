bb9d3582c954a1a9e81781a6f84694b821c1f98d
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-11-13 12:21:59 +0200

    Update githubContentCont.js

--- src/components/githubContentCont.js
+++ src/components/githubContentCont.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Github {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+      } catch (error) {
+      
+      }
+    })()
+  })
 
   return (
     <>
