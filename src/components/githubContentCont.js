ec7d9121539e94283e233bcc662b3ab7b78bf0e5
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-11-16 03:17:40 +0200

    Update githubContentCont.js

--- src/components/githubContentCont.js
+++ src/components/githubContentCont.js
@@ -4,20 +4,6 @@ import { getDatabase } from '.../lib/github';
 export default function Github {
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-        await octokit.request('GET
-        /orgs/{org}/repos', { 
-
-      } catch (error) {
-      
-      }
-    })()
-  })
-
   return (
     <>
       <h1>GitHub Stats</h1>
