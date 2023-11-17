a5a6753a1d5bc87132e469ae6030c23a5148facc
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-11-17 14:53:58 +0200

    Update homeContentCont.js

--- src/components/homeContentCont.js
+++ src/components/homeContentCont.js
@@ -1,8 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './homeContentCont.css';
 import ContentItem from './contentitem';
 
 export default function HomeContentCont() {
+  const [githubRepos, setGithubRepos] = useState([]);
+
+  const fetchGithubRepos = (user) => {
+    return fetch(`https://api.github.com/orgs/${user}/repos`).then((response) => response.json());
+  };
+
+  useEffect(() => {
+    const handleUpdate = async () => {
+      try {
+        const data = await fetchGithubRepos('malebosambo');
+        setGithubData(data);
+      } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+    };
+
+    handleUpdate();
+  }, []);
+  
+  
   const services = [
       {
         id: 1,
@@ -42,20 +62,6 @@ export default function HomeContentCont() {
     ];
   
   const content = services.map((service) => <ContentItem content={service} />);
-
-  useEffect(() => {
-    const handleUpdate = async () => {
-      try {
-        const data = await fetchRepos('malebosambo');
-                              setGithubData(data);
-                                    } catch (error) {
-                                            console.error('Error fetching data:', error);
-                                                  }
-                                                      };
-
-                                                          handleUpdate();
-                                                            }, []);
-  })
 
   return (
     <div className="Container">
