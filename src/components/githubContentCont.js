0ab5a810691c5618b783a328bcf4f0920a8e3ef2
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-11-18 12:22:48 +0000

    fixing warnings

--- src/components/githubContentCont.js
+++ src/components/githubContentCont.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import IssueItem from './issueitem';
+import React, { useState } from 'react';
+
 import './latestIssues.css';
 
 export default function Github() {
