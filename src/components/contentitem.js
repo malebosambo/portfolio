59bbfe66e7709fc242731f3a7bd6aa9fb9b7fa4e
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-11-18 14:08:32 +0200

    Update contentitem.js

--- src/components/contentitem.js
+++ src/components/contentitem.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './homeContentCont.css';
 
-export default function ContentItem() {
-  const { content } = this.props;
+export default function ContentItem({ service }) {
     
   return (
     <>
-      <div className="col-sm-2" style={{margin: "10px"}} key={content.id}>
+      <div className="col-sm-2" style={{margin: "10px"}} key={service.id}>
         <div className="Heading">
-            <h2>{content.name}</h2>
+            <h2>{service.name}</h2>
         </div>
         <hr/>
         <div className="Benefits">
           <p>Services:</p>
-            <ul>{content.ops.map((op) => (<li key={op}>{op}</li>))}</ul>
+            <ul>{service.ops.map((op) => (<li key={op}>{op}</li>))}</ul>
         </div>
         <div className="Image">
-          <img src={content.imgUrl} className="img-responsive img-rounded" alt={content.name} />
+          <img src={service.imgUrl} className="img-responsive img-rounded" alt={service.name} />
         </div>
         <div className="ContentLink">
-          <Link to={content.url}>View more</Link>
+          <Link to={service.url}>View more</Link>
         </div>
       </div>
     </>
