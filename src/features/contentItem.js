2f8a37be687c759ee74c8c2d40097e73ae9fafe0
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-12-09 00:34:48 +0200

    Update contentItem.js

--- src/features/contentItem.js
+++ src/features/contentItem.js
@@ -8,20 +8,22 @@ export default function ContentItem(props) {
  
   return (
     <>
-      <div className="col-sm-2" style={{margin: "10px"}} key={content.id}>
-        <div className="Heading">
-            <h2>{content.name}</h2>
-        </div>
-        <hr/>
-        <div className="Benefits">
-          <p>Services:</p>
-            <ul>{content.ops.map((op) => (<li key={op}>{op}</li>))}</ul>
-        </div>
+      <div className="card col-sm-2" style={{margin: "10px"}} key={content.id}>
         <div className="Image">
-          <img src={content.imgUrl} className="img-responsive img-rounded" alt={content.name} />
+          <img src={content.imgUrl} className="card-img-top img-responsive" alt={content.name} />
+        </div>
+        <div className="card-body">
+          <div className="Heading">
+            <h2>{content.name}</h2>
+          </div>
+          <hr/>
+          <div className="Benefits">
+            <p>Services:</p>
+            <ul>{content.ops.map((op) => (<li key={op}>{op}</li>))}</ul>
+          </div>
         </div>
         <div className="ContentLink">
-          <Link to={content.url}>View more</Link>
+          <Link className="btn btn-primary" to={content.url}>View more</Link>
         </div>
       </div>
     </>
