934c6700f995d308a321af26bdb847568692433d
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-12-08 13:19:34 +0200

    Update curriculumVitae.js

--- src/features/curriculumVitae.js
+++ src/features/curriculumVitae.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+export default function CurriculumViate() {
+
+  return (
+    <>
+      <div className="intro">
+        <div className="avatar">
+          <h1>Malebo Sambo</h1>
+        </div>
+        <div className="basic-details">
+        </div>
+      </div>
+    </>
+  )
+}
