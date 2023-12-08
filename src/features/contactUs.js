d07691a14173a9fe5f5a57d868779c9b5260a855
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-12-08 13:09:27 +0200

    Update contactUs.js

--- src/features/contactUs.js
+++ src/features/contactUs.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-export default ContactUs() {
+export default function ContactUs() {
 
-    return (
-      <>
-        <h1>Contact Us</h1>
-        <div>
-          <h4>General Details</h4>
-          <address>
-            <abbr title="Cellphone">Cell:</abbr> <a href="tel:+27630013711">+27630013711</a>
-          </address>
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <div>
+        <h4>General Details</h4>
+        <address>
+          <abbr title="Cellphone">Cell:</abbr> <a href="tel:+27630013711">+27630013711</a>
+        </address>
           
-          <address>
-            <p>Email:</p> <a href="mailto:sambo.malebo96@gmail.com">sambo.malebo96@gmail.com</a>
-          </address>
+        <address>
+          <p>Email:</p> <a href="mailto:sambo.malebo96@gmail.com">sambo.malebo96@gmail.com</a>
+        </address>
           
-          <address>
-            <p>462 Cuba Crescent, Lulekani, Phalaborwa, 1392</p>
-          </address>
-        </div>
-      </>
-    )
+        <address>
+          <p>462 Cuba Crescent, Lulekani, Phalaborwa, 1392</p>
+        </address>
+      </div>
+    </>
+  )
 }
