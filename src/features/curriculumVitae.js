e3e0568adfd9661e84fea4cbce875bf93daa2497
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2023-12-23 01:49:29 +0200

    Update curriculumVitae.js

--- src/features/curriculumVitae.js
+++ src/features/curriculumVitae.js
@@ -4,11 +4,34 @@ export default function CurriculumVitae() {
 
   return (
     <>
-      <div className="intro">
+      <div className="cv-intro">
         <div className="avatar">
           <h1>Malebo Sambo</h1>
         </div>
-        <div className="basic-details">
+        <div className="contact-details">
+          <h4>Contact Details</h4><br/>
+          <p>Preferred Method</p><br/>
+          <p>Cellphone: 0630013711</p><br/>
+          <p>Alternative Method</p><br/>
+          <p>Email: sambo.malebo96@gmail.com</p><br/>
+          <p>Social Accounts</p><br/>
+          <p>LinkedIn: <span><a href="">Malebo Sambo</a></span></p>
+        </div>
+      </div>
+      <div className="cv-body">
+        <div className="column1">
+          <div className="address-details">
+          </div>
+          <div className="education-details">
+          </div>
+        </div>
+        <div className="column2">
+          <div className="personal-details">
+          </div>
+          <div className="specializations">
+          </div>
+        </div>
+        <div className="personal-profile">
         </div>
       </div>
     </>
