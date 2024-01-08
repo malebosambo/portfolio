abb2f71879275715d0684cb1239e8434ef61dc8a
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-01-08 12:48:53 +0200

    Update repoItem.js

--- src/features/repoItem.js
+++ src/features/repoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function RepoItem({ id, name, description, date, topics }) {
 
   return (
     <>
       <tr key={id}>
-        <td>{name}</td>
+        <td><Link href="/repo/{name}">{name}</Link></td>
         <td>{description}</td>
         <td>{date}</td>
         <td>{topics}</td>
