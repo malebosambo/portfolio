3f748859972cf380491e805772499affb4fddbd8
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-02-07 11:56:34 -0800

    Update issueItem.js

--- src/features/issueItem.js
+++ src/features/issueItem.js
@@ -1,8 +1,15 @@
-export default function IssueItem() {
+import React from 'react';
+
+export default function IssueItem({ id, title, description, date, status }) {
 
   return (
     <>
-
+      <tr key={id}>
+        <td>{title}</td>
+        <td>{description}</td>
+        <td>{date}</td>
+        <td>{status}</td>
+      </tr>
     </>
   )
 }
