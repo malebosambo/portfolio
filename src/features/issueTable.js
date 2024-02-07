788254f18748ff66e8f79050e031885ec8e85e9f
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-02-07 09:58:38 -0800

    Update issueTable.js

--- src/features/issueTable.js
+++ src/features/issueTable.js
@@ -1,10 +1,40 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function IssueTable() {
+export default function IssueTable( repo ) {
+
+  const [issues,setIssues] = useState([]);
+
+  useEffect(() => {
+    const fetchRepoIssues = async (username, reponame) => {
+
+      const issueResponse = await fetch(`https://api.github.com/repos/${username}/${reponame}/issues`);
+
+      const issueData = await response.json();
+
+      setIssues(issueData);
+    };
+
+    fetchRepoIssues('malebosambo', repo);
+  }, []); 
+
 
   return (
     <>
-
+      <div className="TableResponse">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Created Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {issues.map((issue) => <IssueItem key={issue.id} title={issue.title} description={issue.body} date={issue.created_at} status={issue.state} />)}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
