8e23c867167a64838e1d712cb3174b926bbcf9d4
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-02-07 17:10:40 +0200

    Update viewRepo.js
    Fetching individual repo and issues using the name request parameter

--- src/features/viewRepo.js
+++ src/features/viewRepo.js
@@ -1,7 +1,29 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 export function viewRepo() {
 
+  const [repo,setRepo] = useState({});
+  const [issues,setIssues] = useState([]);
+  const { repo } = useParams();
+
+  useEffect(() => {
+    const fetchRepo = async (username) => {
+      const repoResponse = await fetch(`https://api.github.com/users/${username}/repos`);
+      const repoData = await response.json();
+      setRepo(repoData);
+    };
+
+    const fetchRepoIssues = async (username, reponame) => {
+      const issueResponse = await fetch(`https://api.github.com/repos/${username}/${reponame}/issues`);
+      const issueData = await response.json();
+      setIssues(issueData);
+    };
+
+    fetchRepo('malebosambo');
+    fetchRepoIssues('malebosambo', repo);
+  }, []);  
+  
   return (
 
   )
