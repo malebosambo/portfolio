17f06c728ff6a8f7b172b1964bbea9230c7f8932
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-02-23 20:41:58 -0800

    Update viewRepo.js

--- src/features/viewRepo.js
+++ src/features/viewRepo.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import IssueTable from './issueTable';
 
-export function viewRepo() {
+export default function viewRepo() {
 
   const [repo,setRepo] = useState({});
   
