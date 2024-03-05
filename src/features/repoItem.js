e46274f43503c7e517ecf582301e0c43935033f9
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-03-05 05:41:22 -0800

    Update repoItem.js

--- src/features/repoItem.js
+++ src/features/repoItem.js
@@ -6,7 +6,7 @@ export default function RepoItem({ id, name, description, date, topics }) {
   return (
     <>
       <tr key={id}>
-        <td><Link href="/repo/{name}">{name}</Link></td>
+        <td><Link to="/repo/{name}">{name}</Link></td>
         <td>{description}</td>
         <td>{date}</td>
         <td>{topics}</td>
