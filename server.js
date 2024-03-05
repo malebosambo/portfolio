5b83551f9d5780546fdba1ff35eb86bed2684888
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-03-05 02:30:05 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 const port = 4000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 app.get("/", async (req, res) => {
   res.json({ "message": "OK" });
 });
