4efbc93ea6c99ab76c14129d6d7e17b00dcc9d1e
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-03-06 10:19:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   res.json({ "message": "OK" });
