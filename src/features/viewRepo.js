eaa5c47e1e349e3c2faa60d2888b4007bc5aa5a4
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-03-10 22:49:10 -0700

    Update viewRepo.js

--- src/features/viewRepo.js
+++ src/features/viewRepo.js
@@ -1,28 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import IssueTable from './issueTable';
 
 export default function ViewRepo() {
-
-  /*
-  const [repo,setRepo] = useState({});
-  
-  const { name } = useParams();
-
-  useEffect(() => {
-    const fetchRepo = async (username, reponame) => {
-
-      const response = await fetch(`https://api.github.com/repos/${username}/${reponame}`);
-
-      const repoData = await response.json();
-
-      setRepo(repoData);
-    };
-
-    fetchRepo('malebosambo', name);
-    
-  }, {});  
-  */
 
   return (
     <>
@@ -30,7 +10,7 @@ export default function ViewRepo() {
         <h1>Repo Name</h1>
       </div>
       <div>
-       {/* <IssueTable name={repo.name} /> */}
+      
       </div>
     </>
   )
