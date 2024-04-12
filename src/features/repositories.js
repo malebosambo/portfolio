753cfbb55f0bd986d865ee615d5d5769256e6ab8
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-04-11 18:11:10 -1200

    Update repositories.js

--- src/features/repositories.js
+++ src/features/repositories.js
@@ -6,15 +6,18 @@ export default function Repositories() {
   return (
     <>
       <div>
-        <h1>@malebosambo</h1>
-      </div>
-
-      <div className="RepoFilters">
-        <div className="btn-group">
-          <button className="btn btn-primary m-2" onClick={() => this.filterIssues("websites")}>Websites</button>
-          <button className="btn btn-primary m-2" onClick={() => this.filterIssues("apps")}>Apps</button>
+        <div>
+          <h1>GitHub Repos</h1>
         </div>
-      </div>
+
+        {/*
+        <div className="RepoFilters">
+          <div className="btn-group">
+            <button className="btn btn-primary m-2" onClick={() => this.filterIssues("websites")}>Websites</button>
+            <button className="btn btn-primary m-2" onClick={() => this.filterIssues("apps")}>Apps</button>
+          </div>
+        </div>
+        */}
 
       <div className="RepoTable">
         <RepoTable />
