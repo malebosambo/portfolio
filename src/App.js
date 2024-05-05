c53e2b1db1e9933a03d0d8182152d75c3d471947
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-05-05 11:05:28 +0000

    navbar design

--- src/App.js
+++ src/App.js
@@ -9,22 +9,19 @@ import Issues from './features/issueTable';
 import WebsiteApps from './features/websiteApps';
 import MobileApps from './features/mobileApps';
 import ContactUs from './features/contactUs';
+import Nav from './components/navbar';
+
 import Footer from './components/footer';
 
 export function App() {
   return (
     <div>
       <div className="Header">
-        <div className="Logo">
-          <h4>Malebo Sambo</h4>
-        </div>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/cv-resume">CV</Link>
-          <Link to="/repos">Repos</Link>
-          <Link to="/issues">Issues</Link>
-          <Link to="/contacts">Contacts</Link>
-        </div>  
+        <Nav />
+
+
+
+
       </div> 
       <Home />
       <Footer />
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <>
       <div className="Logo">
-        <img src="" atl="malebo-sambo-logo" />
+        <img src="./Malebo-Sambo-Logo.jpeg" atl="malebo-sambo-logo" className="Logo" />
         <button className="Mobile-Menu"><img src="" alt="mobile-menu" /></button>
         <button className="Mobile-Menu-Close"><img src="" alt="mobile-menu-close" /></button>
       </div>
