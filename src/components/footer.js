dd494cb3820a85525425f7c69e3e5b9c65a3d024
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-05-05 14:54:50 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,15 +7,18 @@ export default function Footer() {
     <>
       <div className="Footer">
         <div className="Legal">
+          <h5>Legal</h5><br />
           <p>Copyright © 2024</p>
           <p>Malebo Sambo</p>
           <p>All rights reserved.</p>
         </div>
         <div className="Shortlinks">
+          <h5><span><i className="bi bi-file-person"></></span>Contacts</h5><br />
           <p>Email: help@malebo.sambo.com</p>
           <p>Cell: +27630013711</p>
         </div>
         <div className="Socials">
+          <h5><span><i className="bi bi-file-person"></></span>Socials</h5><br />
           <a href="www.malebosambo.github.app"><img src="" alt="portfolio" /></a>
           <a href="www.linkedin.com/malebosambo"><img src="" alt="LinkedIn" /></a>
           <a href="www.github.com/sambomalebo"><img src="" alt="GitHub" /></a>
