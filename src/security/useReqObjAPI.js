3db9fba918db5ac7b955afc262c78b61128e9063
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-05-07 11:25:31 +0200

    Update useReqObjAPI.js

--- src/security/useReqObjAPI.js
+++ src/security/useReqObjAPI.js
@@ -1,1 +1,29 @@
-import
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export function useRequestAPI(url) {
+  const [data, setData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = () => {
+      setIsLoaded(true);
+
+      axios.get(url).then((response) => {
+        setData(response.data);
+
+        setIsLoaded(false);
+      })
+       .catch((error) => {
+         setError(error.message);
+
+         setIsLoaded(false);
+      });
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoaded, error };
+};
