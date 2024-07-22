bfdc1ea74bda434808ca017a58a2e3aa0d0de974
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-07-22 09:27:58 +0000

    update design: adding functionality to the navbar for mobile view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
 
-  const { mobileMenu, setMobileMenu } = useState(false);
+  const [ mobileMenu, setMobileMenu ] = useState(false);
 
   const showMenu = () => setMobileMenu(!mobileMenu);
 
@@ -11,12 +11,12 @@ export default function Navbar() {
     <>
       <div className="Logo">
         <div><img src="./Malebo-Sambo-Logo-Transp.png" atl="malebo-sambo-logo" className="Logo" /></div>
-        <div>
-          <button onClick={showMenu} className="Mobile-Menu">=</button>
-          <button onClick={showMenu} className="Mobile-Menu-Close">X</button>
+        <div style={{padding: "20px"}}>
+          <button onClick={showMenu} className={ !mobileMenu ? "" : "Menu" }>=</button>
+          <button onClick={showMenu} className={ mobileMenu ? "" : "Menu" }>X</button>
         </div>  
       </div>
-      <div className="Menu">
+      <div className={ mobileMenu ? "Menu-Visible" : "Menu" }>
         <Link to="/">Home</Link>
         <Link to="/cv-resume">CV</Link>
         <Link to="/repos">Repos</Link>
