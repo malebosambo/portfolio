f7c7da8aff6e239b6aa6c2479164ea4a1c022bd9
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-08-24 11:43:57 +0200

    Update repoItem.js

--- src/features/repoItem.js
+++ src/features/repoItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function RepoItem({ id, name, description, date, topics }) {
 
   const repoName = `/repo/${name}`;
+  const formatDate = Intl.DateTimeFormat("en-US", { year: "numeric", month: "2-digit", day: "2-digit" }).format(date);
 
   return (
     <>
       <tr key={id}>
         <td><Link to={repoName}>{name}</Link></td>
         <td>{description}</td>
-        <td>{date}</td>
+        <td>{formatDate}</td>
         <td>{topics}</td>
       </tr>
     </>
