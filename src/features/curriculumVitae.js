56bf1ee20a6f6b7abe32495541c801c097470bdb
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-10-13 01:50:54 +0200

    Update curriculumVitae.js

--- src/features/curriculumVitae.js
+++ src/features/curriculumVitae.js
@@ -6,10 +6,10 @@ export default function CurriculumVitae() {
     <>
       <main className="CV">
 
-        <div className="CV-Contacts">
-          <div className="CV-Overview">
+        <div className="CV_Contacts">
+          <div className="CV_Overview">
             <div className="Avatar">
-              <img src="./Malebo-Sambo.png" alt="Malebo-Sambo-Avatar" className="Avatar" />
+              <img src="./Malebo-Sambo.png" alt="Avatar" className="Avatar" />
             </div>
             <div className="Profile">
               <br />
@@ -28,36 +28,36 @@ export default function CurriculumVitae() {
               <a href="www.twitter.com/lovais000"><img src="./Twitter.png" alt="Twitter" className="Icon" /></a>
             </div>  
           </div>
-          <div className="CV-Email">
+          <div className="CV_Email">
             <img src="Email.png" className="Icon" />
             <div>
               <h4>Email</h4>
               <a href="mailto:sambo.malebo96@gmail.com"><p>sambo.malebo96@gmail.com</p></a>
             </div>
           </div>
-          <div className="CV-Phone">
+          <div className="CV_Phone">
             <img src="Telephone.png" className="Icon" />
             <div>
               <h4>Phone Number</h4>
               <a href="tel:+27630013711"><p>+27 63 001 3711</p></a>
             </div>
           </div>
-          <div className="CV-Socials">
-            <a href="www.linkedin.com/malebosambo"><img src="./Linkedin.png" alt="LinkedIn" className="Social-Logo" /></a>
-            <a href="www.github.com/sambomalebo"><img src="./Github.png" alt="GitHub" className="Social-Logo" /></a>
-            <a href="www.twitter.com/lovais000"><img src="./Twitter.png" alt="Twitter" className="Social-Logo" /></a>
+          <div className="CV_Socials">
+            <a href="www.linkedin.com/malebosambo"><img src="./Linkedin.png" alt="LinkedIn" className="Social_Logo" /></a>
+            <a href="www.github.com/sambomalebo"><img src="./Github.png" alt="GitHub" className="Social_Logo" /></a>
+            <a href="www.twitter.com/lovais000"><img src="./Twitter.png" alt="Twitter" className="Social_Logo" /></a>
           </div>
         </div>
 
-        <div className="CV-Body">
-          <div className="CV-Section1">
-            <div className="CV-About">
+        <div className="CV_Body">
+          <div className="CV_Section1">
+            <div className="CV_About">
               <h4>Personal Summary</h4><br />
               <p>I am an IT professional with experience in both Software and Mobile App Development, Quality Assuarance (QA), Research & Development (R&D) Analysis, and Business Analysis (BA). </p><br />
               <p>I have skills and experience as a Operations Manager for SMMEs in the IT sector, I also provide organizational culture and business model development expertise.</p><br />
               <p>I possess great personal characteristics that assist me to work with diverse and incredible private and public sector individuals to deliver projects and initiatives.</p>
             </div>
-            <div className="CV-Education">
+            <div className="CV_Education">
               <h4>Education</h4><br />
               <div>
                 <p>2010-2014</p>
@@ -70,7 +70,7 @@ export default function CurriculumVitae() {
                 <p>OutSystems</p>
               </div>
             </div>
-            <div className="CV-Softskills">
+            <div className="CV_Softskills">
               <h4>Soft Skills</h4><br />
               <ul>
                 <li>Communication (Verbal/Written)</li>
@@ -79,13 +79,13 @@ export default function CurriculumVitae() {
             </div> 
           </div>
 
-          <div className="CV-Section2">
-            <div className="CV-Avatar">
-              <img src="./CVAvatar.jpg" alt="avatar" />
+          <div className="CV_Section2">
+            <div className="CV_Avatar">
+              <img src="./Malebo-Sambo.png" alt="Avatar" />
               <h1>Malebo Sambo</h1>
               <h3>Software Developer/Engineer</h3>
             </div>
-            <div className="CV-Hardskills">
+            <div className="CV_Hardskills">
               <h4>Hard Skills</h4><br />
               <ul>
                 <li>OutSystems Reactive Developer</li>
@@ -97,8 +97,8 @@ export default function CurriculumVitae() {
             </div>
           </div>
 
-          <div className="CV-Section3">
-            <div className="CV-Employment">
+          <div className="CV_Section3">
+            <div className="CV_Employment">
               <h4>Experience</h4><br />
               <div>
                 <p>2021-2022</p>
