634c37ab7ff77c568a708dd242243cfd673f1a30
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-10-21 11:38:04 +0200

    Update repoCard.js

--- src/features/repoCard.js
+++ src/features/repoCard.js
@@ -1,10 +1,15 @@
-<div className="card">
-  <div className="card-header">
-    Featured
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+import React from "react";
+
+export async function RepoCard({ name, description, link, }) {
+
+  return (
+    <div className="card">
+      <div className="card-header">Trending</div>
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+        <a target="_blank" rel="noopener noreferrer" href={link} className="btn btn-primary">View</a>
+      </div>
+    </div>
+  );
+}
