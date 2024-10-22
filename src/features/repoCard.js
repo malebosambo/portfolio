80aa0ba3bea0d1a608b4288b3c6c2a8b0a46c2cc
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-10-22 09:52:23 +0200

    Update repoCard.js

--- src/features/repoCard.js
+++ src/features/repoCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export async function RepoCard({ name, description, link, }) {
+export async function RepoCard({ name, description, issues, link, }) {
 
   return (
     <div className="card text-bg-warning mb-3">
       <div className="card-header">Trending</div>
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
+        <p>Open Issues: {issues}</p>
         <p className="card-text">{description}</p>
         <a target="_blank" rel="noopener noreferrer" href={link} className="btn btn-primary">View</a>
       </div>
